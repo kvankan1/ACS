847456861bcb2a7334f8d460c11306fc750b7e5c
kvankan1 <103990140+kvankan1@users.noreply.github.com>
2023-12-15 20:44:19 +0100

    Added a function to search subspace for optimal solution

--- main.py
+++ main.py
@@ -135,6 +135,49 @@ def PrintYLoadings(cca, X_keys, y_keys):
             file.write(f"{y_keys[i]:<{column_width}}\t" + "\t".join(f"{value:.3f}".ljust(column_width) for value in y_loadings[i]) + "\n")
 
 
+# Define the Optimize function
+def Optimize(X_train, Y_train, X_predict, y_keys):
+    n_components = len(y_keys)
+
+    # Fit the CCA model
+    cca = CCA(n_components = n_components)
+    cca.fit(X_train, Y_train)
+
+    # Predict using the CCA model for all candidates
+    y_pred_candidates = cca.predict(X_predict)
+
+    # Extract the predicted values for the constraints
+    temp_in_turbo = y_pred_candidates[:, 1]  # Assuming temp_in_turbo is the second column
+    in_cylinder_max_pressure = y_pred_candidates[:, 2]  # Assuming in_cylinder_max_pressure is the third column
+    max_compressor_pressure = y_pred_candidates[:, 5]  # Assuming max_compressor_pressure is the sixth column
+    knock_limited_mass = y_pred_candidates[:, 4]  # Assuming knock_limited_mass is the fifth column
+
+    # Check if constraints are met for each candidate
+    constraints_met = (
+        (temp_in_turbo <= 1223.15) &
+        (in_cylinder_max_pressure <= 140.0) &
+        (max_compressor_pressure <= 2.5) &
+        (knock_limited_mass <= 164.0)
+    )
+
+    # Filter candidates that meet all constraints
+    valid_candidates = y_pred_candidates[constraints_met]
+
+    # If there are valid candidates, find the one with the lowest BSFC
+    if len(valid_candidates) > 0:
+        # Get indices of valid candidates
+        optimal_index = np.argmin(valid_candidates[:,3])
+
+        # Get the optimal solution
+        optimal_solution = valid_candidates[optimal_index]
+        optimal_parameters = X_optimize[optimal_index]
+    else:
+        # If there are no valid candidates, set optimal_solution to None
+        optimal_solution = None
+        optimal_parameters = None
+
+    return optimal_solution, optimal_parameters
+
 
 ##=========================================================Now We Use the Functions with the Input Data============================================================#
     
@@ -142,35 +185,86 @@ def PrintYLoadings(cca, X_keys, y_keys):
 correlation_matrix, remaining_keys = GetCorrelationMatrix(Data)
 PrintCorrelationMatrix(remaining_keys, correlation_matrix)
 
-# Assuming Data is a dictionary
-#print(Data.keys())
+
+##===========================================================BASIC SET UP OF THE DATA===========================================================================#
+
+#We define, y_keys, used to create a CCA object to study only some keys, and y_keys_all, to keep track of all the outputs, and to use later in the optimize function
 
 # Extracting data from the dictionary using keys
 X_keys = ['Stroke/Bore', 'Volumetric coefficient', 'Compression ratio', 'norm. TKE', 'SA', 'Water inj.', 'EIVC']    #Choose x_keys according to which inputs you want to include
+y_keys_all = ["Torque [Nm]", "Temp in Turbo [K]", "In cylinder max Pressure [bar]", 'BSFC [g/kwH]',"Knock mass [mg]", "Max compressor pressure [bar]", "BMEP [bar]"]  #All outputs
 y_keys = ['BSFC [g/kwH]', "Temp in Turbo [K]", "In cylinder max Pressure [bar]"]  #Choose y_keys according to which outputs you want to model
 
+
 # Extracting data using keys, Data is taken from input_data.py
+#In this case, y is created from all the y_keys, such that all output data are placed in y
+
 X = np.array([Data[key] for key in X_keys])
-y = np.array([Data[key] for key in y_keys])
+y = np.array([Data[key] for key in y_keys_all])
 
 # Transposing the arrays for the correct shape (samples x features)
 X = X.T
 y = y.T
 
 # Initialize a CCA object
-n_components = 3
+#n_components should match number of keys, right now all outputs are studied
+n_components = 7
 cca = CCA(n_components)
 
 # Fit the CCA model
 cca.fit(X, y)
 
 
-#Print all equations, weights and loadings
-PrintEquation(cca, X_keys, y_keys)
-PrintXWeights(cca, X_keys, y_keys)
-PrintYWeights(cca, X_keys, y_keys)
-PrintXLoadings(cca, X_keys, y_keys)
-PrintYLoadings(cca, X_keys, y_keys)
+#Print all equations, weights and loadings, functions are called from above
+PrintEquation(cca, X_keys, y_keys_all)
+PrintXWeights(cca, X_keys, y_keys_all)
+PrintYWeights(cca, X_keys, y_keys_all)
+PrintXLoadings(cca, X_keys, y_keys_all)
+PrintYLoadings(cca, X_keys, y_keys_all)
+
+
+
+#========================================================NOW THE OPTIMIZER=============================================================#
+##Now the Optimizer, we first define the subspace of input values to be studied, this is done below, where minimum and maximum values
+#of each input parameter are given, along with the amount of data points. This should be the same for all data. Then reshaped
+
+n_data = 100
+
+# Stroke to Bore Ratio
+sbr = np.linspace(0.8, 1.3, n_data).reshape(-1, 1)
+
+# Volumetric Coefficient
+volumetric_coefficient = np.linspace(1.4, 2.2, n_data).reshape(-1, 1)
+
+# Compression Ratio
+compression_ratio = np.linspace(8, 14, n_data).reshape(-1, 1)
+
+# Norm. TKE
+norm_tke = np.linspace(0.8, 1.5, n_data).reshape(-1, 1)
+
+# Spark Advance
+spark_advance = np.linspace(0, 60, n_data).reshape(-1, 1)
+
+# Water Injection
+water_injection = np.linspace(0, 50, n_data).reshape(-1, 1)
+
+# EIVC
+eivc = np.linspace(165, 230, n_data).reshape(-1, 1)
+
+# Stack all arrays vertically
+X_optimize = np.hstack((sbr, volumetric_coefficient, compression_ratio, norm_tke, spark_advance, water_injection, eivc))
+
+
+#Finally call the optimizer function and print any results
+
+opt = Optimize(X_train = X, Y_train = y, X_predict = X_optimize, y_keys = y_keys_all)
+
+print(f"Optimal BSFC = {opt[0]}, Optimal Parameters = {opt[1]}")
+
+
+
+
+
 
 
 
